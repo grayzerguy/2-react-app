5bc72c894b6eb8fb0dd378deb7065eb866d006de
grayzerguy <grayzerg@gmail.com>
2024-04-16 16:27:33 +0300

    event handel

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,34 @@
-
+import { MouseEvent } from "react"; //MouseEvent from "react";
 
 function ListGroup() {
 
-    return (
+    let items = ["New York", "London", "Tokyo", "Paris"];
+    // items = []
 
-        <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-        </ul>
+    // if (items.length === 0)
+    //     return (<>
+    //         <h1>LIST</h1>
+    //         <p>No data</p>
+    //     </>)
+//event hedeler 
+    const handelClick = (event : MouseEvent) => console.log(event)
+
+    return (
+        <>
+            <h1>List Group</h1>
+            {items.length === 0 && <p>No items found</p>}
+            <ul className="list-group">
+                {items.map((item, index) => (
+                    <li
+                        key={item}
+                        className="list-group-item"
+                        onClick={handelClick}
+                    >{item}
+
+
+                    </li>))}
+            </ul>
+        </>
     )
 }
 
